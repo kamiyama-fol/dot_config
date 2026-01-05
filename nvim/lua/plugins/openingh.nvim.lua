a36c08c61b653706831910ba998cc45b2bed64a5
Kamiyama <kueyama@netprotections.co.jp>
2026-01-05 10:05:18 +0900

    Revert "snacks.nvimを解体"
    This reverts commit bc1356ac6b2125c6e0188a167ebd717bba7f6dfb.

--- nvim/lua/plugins/openingh.nvim.lua
+++ nvim/lua/plugins/openingh.nvim.lua
@@ -4,7 +4,6 @@ return{
     { "<Leader>gr", ":OpenInGHRepo<CR>", mode = "n", { silent = true, noremap = true }, desc = "GitHubリポジトリを開く" },
     { "<Leader>gf", ":OpenInGHFile<CR>", mode = "n", { silent = true, noremap = true }, desc = "GitHubでファイルを開く" },
     { "<Leader>gf", ":OpenInGHFileLines<CR>", mode = "v", { silent = true, noremap = true }, desc = "GitHubで選択行を開く" },
-    { "<leader>gB", ":OpenInGHFile<CR>", mode = "n", { silent = true, noremap = true }, desc = "Git Browse (GitHub)" },
   }
 
 }
